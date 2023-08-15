ef1dc802bd507ac7b88f60e29eef57e8dd1f5a49
ZYS <52597357+ZYS-GITHUB@users.noreply.github.com>
2023-08-15 18:10:33 +0800

    111
    222

---
+++ main.c
@@ -0,0 +1,104 @@
+/*!
+    \file    main.c
+    \brief   GPIO keyboard polling demo
+
+    \version 2017-06-28, V1.0.0, demo for GD32F3x0
+    \version 2019-06-01, V2.0.0, demo for GD32F3x0
+    \version 2021-05-31, V2.1.0, demo for GD32F3x0
+    \version 2022-03-06, V2.2.0, demo for GD32F3x0
+*/
+
+/*
+    Copyright (c) 2022, GigaDevice Semiconductor Inc.
+
+    Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this 
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice, 
+       this list of conditions and the following disclaimer in the documentation 
+       and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holder nor the names of its contributors 
+       may be used to endorse or promote products derived from this software without 
+       specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+OF SUCH DAMAGE.
+*/
+
+
+#include "myinit.h"
+
+void led_flash(int times);
+
+/*!
+    \brief      main function
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+int main(void)
+{
+    /* initialize KEY and LED, configure SysTick */
+
+
+
+    systick_config();
+//		gd_eval_com_init(EVAL_COM);    
+
+
+    /* flash the LED for test */
+    led_flash(1);
+//    /* print out the clock frequency of system, AHB, APB1 and APB2 */
+    printf("\r\nCK_SYS is %d", rcu_clock_freq_get(CK_SYS));
+    printf("\r\nCK_AHB is %d", rcu_clock_freq_get(CK_AHB));
+    printf("\r\nCK_APB1 is %d", rcu_clock_freq_get(CK_APB1));
+    printf("\r\nCK_APB2 is %d", rcu_clock_freq_get(CK_APB2));
+		printf("\r\n");
+		printf("BOOT setup\r\n");
+		updaterun();//检查更新
+    while(1){
+			
+//		delay_1ms(100);            
+//		gpio_bit_toggle(GPIOB, GPIO_PIN_5);                  
+    }
+}
+
+/*!
+    \brief      flash the LED for test
+    \param[in]  times: times to flash the LEDs
+    \param[out] none
+    \retval     none
+*/
+void led_flash(int times)
+{
+    int i;
+    for(i = 0;i < times;i++){
+        /* delay 250 ms */
+        delay_1ms(250);
+        /* toggle the LED */ 
+        gpio_bit_toggle(GPIOB, GPIO_PIN_5);
+
+        /* delay 250 ms */
+        delay_1ms(250);
+        /* toggle the LED */ 
+        gpio_bit_toggle(GPIOB, GPIO_PIN_5);
+    }
+}
+/* retarget the C library printf function to the USART */
+int fputc(int ch, FILE *f)
+{
+    usart_data_transmit(EVAL_COM, (uint8_t)ch);
+    while(RESET == usart_flag_get(EVAL_COM, USART_FLAG_TBE));
+
+    return ch;
+}
